fa2143a6b7aba2ef9e23b8499e56c2e3b0bcac8a
abalozz <alivanprox@gmail.com>
2014-03-03 21:07:47 +0100

    Diseño de la sección principal

--- views/home.php
+++ views/home.php
@@ -1,23 +1,49 @@
 {{ extends 'layouts/private' }}
 
-<p>Usuario: <?php echo $user->get_username() ?></p>
-<h3><?php echo $mensajos ?></h3>
+<header>
+    <div id="user-media">
+        <img src="https://pbs.twimg.com/profile_banners/437581554/1393030827/web"
+            alt="Cabecera" class="user-header-img">
+        <img src="https://pbs.twimg.com/profile_images/3614570769/cd11aed39c0c64e296d00c7bd827bb3a.jpeg"
+            alt="<?php echo $user->get_username() ?>" class="user-profile-img">
+        <h1 class="name"><?php echo $user->get_username() ?> <span class="username">(<?php echo $user->get_username() ?>)</span></h1>
+    </div>
+    <div id="user-stats">
+        <a href="?page=users&amp;action=show&amp;id=<?php echo $user->get_id() ?>" id="mensajos" class="stat">
+            <span class="text">Mensajos</span>
+            <span class="count">0</span>
+        </a
+        ><a href="?page=users&amp;action=followers&amp;id=<?php echo $user->get_id() ?>" id="followers" class="stat">
+            <span class="text">Seguidores</span>
+            <span class="count"><?php echo $user->get_number_of_followers() ?></span>
+        </a
+        ><a href="?page=users&amp;action=following&amp;id=<?php echo $user->get_id() ?>" id="following" class="stat">
+            <span class="text">Siguiendo</span>
+            <span class="count"><?php echo $user->get_number_of_followings() ?></span>
+        </a>
+    </div>
+</header>
 
-<form action="?page=messages&amp;action=store" method="post">
-    <textarea name="mensajo" cols="30" rows="10" placeholder="Envía un nuevo mensajo"></textarea>
-    <button>Enviar</button>
-</form>
+<div id="send-mensajo-box">
+    <form action="?page=messages&amp;action=store" method="post">
+        <textarea name="mensajo" cols="30" rows="10" placeholder="Envía un nuevo mensajo"></textarea>
+        <button>Enviar</button>
+    </form>
+</div>
 
-<div>
+<section id="messages">
     <?php if (count($messages)): ?>
         <?php foreach ($messages as $message): ?>
-            <article>
+            <article class="message">
+                <img src="https://pbs.twimg.com/profile_images/3614570769/cd11aed39c0c64e296d00c7bd827bb3a.jpeg"
+                    alt="<?php echo $message->get_username() ?>" class="profile-img">
+                <a href="?page=users&amp;action=show&amp;id=<?php echo $message->get_user_id() ?>"><?php echo $message->get_username() ?></a>
                 <p>
-                    <span><?php echo $message->get_username() ?>:</span> <?php echo $message->get_content() ?>
+                    <?php echo $message->get_content() ?>
                 </p>
             </article>
         <?php endforeach; ?>
     <?php else: ?>
         No tienes mensajes que ver :(
     <?php endif; ?>
-</div>
+</section>
--- views/layouts/private.php
+++ views/layouts/private.php
@@ -5,17 +5,30 @@
     <title><?php echo $title ?></title>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
 
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/normalize.css">
+
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/header-user.css">
+    <link rel="stylesheet" href="css/send-box.css">
+    <link rel="stylesheet" href="css/messages.css">
 </head>
 <body>
 
-    <ul>
-        <li><a href="./">Inicio</a></li>
-        <li><a href="?page=users">Lista de usuarios</a></li>
-        <li><a href="?page=home&amp;action=logout">Cerrar sesión</a></li>
-    </ul>
+    <nav>
+        <div class="max-nav-width">
+            <a href="./">Inicio</a>
+            <a href="?page=profile">Perfil</a>
+            <div class="nav-right">
+                <a href="?page=settings">Ajustes</a>
+                <a href="?page=home&amp;action=logout">Cerrar sesión</a>
+            </div>
+        </div>
+    </nav>
 
-    <?php echo $child ?>
+    <div id="wrap">
+        <?php echo $child ?>
+    </div>
 </body>
 </html>
