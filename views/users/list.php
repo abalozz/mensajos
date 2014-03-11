ac4d6147184b24771a83534a8f9df51e82f269f0
abalozz <alivanprox@gmail.com>
2014-03-11 22:35:13 +0100

    Diseño de la lista de usuarios

--- views/users/list.php
+++ views/users/list.php
@@ -1,7 +1,22 @@
 @extends 'layouts/private'
 
-<ul>
+<section id="users-list">
     <?php foreach ($users as $user): ?>
-        <li><a href="?page=users&amp;action=show&amp;id=<?php echo $user->get_id() ?>"><?php echo $user->get_username() ?></a></li>
+        <article class='user'>
+            <a href="?page=users&amp;action=show&amp;id=<?php echo $user->get_id() ?>">
+                <img src="<?php echo $user->get_profile_image() ?>" alt="<?php $user->get_name() ?>" class="profile-image">
+                <h2 class="name">
+                    <?php echo $user->get_name() ?>
+                </h2>
+                <span class="username"><?php echo $user->get_username() ?></span>
+            </a>
+            <a href="?page=users&amp;action=follow&amp;id=<?php echo $user->get_id() ?>" class="btn follow">
+                <?php if (Auth::user()->is_following_to($user)): ?>
+                    Dejar de seguir
+                <?php else: ?>
+                    Seguir
+                <?php endif; ?>
+            </a>
+        </article>
     <?php endforeach; ?>
-</ul>
+</section>
